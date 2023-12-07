95db3584398e1f07f130f2f3f598062ea07fbc3b
Zucker <muhammedkarakuza@gmail.com>
2023-12-07 19:55:40 +0300

    productcard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import ProductCard from "./components/ProductCard/ProductCard";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ProductCard />
     </div>
   );
 }
---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,25 @@
+import { products } from "../../helper/data";
+import "./ProductCard.css";
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div className="cards">
+      {products.map((item) => (
+        <div className="card-main">
+          <div className="card-img">
+            <img src={item.image} alt="product" />
+          </div>
+          <div className="card-body">
+            <h3>{item.title}</h3>
+          </div>
+          <div className="price">
+            <h4>{item.price}</h4>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductCard;
