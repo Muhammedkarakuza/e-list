bfa175486aed7a7b1ccbb4d697f11d1322461c05
Zucker <muhammedkarakuza@gmail.com>
2023-12-07 19:33:27 +0300

    data added and header.jsx completed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { categories } from "../../helper/data";
+
+const Header = () => {
+  return (
+    <div className="header-main">
+      <h1>Product List</h1>
+      <div className="btn-pro">
+        {categories.map((item) => (
+          <button>{item.toUpperCase()}</button>
+        ))}
+      </div>
+    </div>
+  );
+};
